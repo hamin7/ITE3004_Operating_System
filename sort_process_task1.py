1d82049230031bc63bbdb451beba25e64047e64d
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-06-04 20:30:02 +0900

    Update sort_process_task1.py

--- sort_process_task1.py
+++ sort_process_task1.py
@@ -7,17 +7,17 @@ def sorting_thread(array, pid) :
     
     print('#############Thread #%d started...............\n' %pid)
 
-    start = timeit.default_timer()
-    random_function(array)
-    sorting_function(array)
-    print_array(array, pid)
-    stop = timeit.default_timer()
-    print('%0.3f sec' %(stop-start))
+    start = timeit.default_timer()          # start the timer.
+    random_function(array)          # function that randomly pick 10 numbers.
+    sorting_function(array)         # function that sort numbers.
+    print_array(array, pid)         # function that print sorted numbers.
+    stop = timeit.default_timer()       # stop the timer.
+    print('%0.3f sec' %(stop-start))        # time to sort.
         
 def random_function(array):
     i=0
     for i in range(10):
-        array[i]=random.randrange(0,100)
+        array[i]=random.randrange(0,100)        # randomly pick the number in range 1~100.
     return    
 
 def sorting_function(array) :
@@ -25,60 +25,24 @@ def sorting_function(array) :
     for i in range(9,0,-1):
         j=0
         for j in range(i):
-            time.sleep(0.01)
-            if array[j] > array[j+1] :
+            time.sleep(0.01)            # in order to slow down.
+            if array[j] > array[j+1] :      # swap the bigger one and smaller one.
                 tmp = array[j+1]
                 array[j+1]=array[j]
                 array[j]=tmp
     return            
 
 def print_array(array, pid):
-    print"[%d] --> "  %pid,; 
+    print"[%d] --> "  %pid,;            # print the process's id. 
     for i in range(10):
-        print"%d " %array[i],;
+        print"%d " %array[i],;              # print sorted numbers.
 
 if __name__ =='__main__' :
     array=multiprocessing.Array('i',[0]*10)
     print('############Start#############')
     
     for i in range(10) :
-        p = multiprocessing.Process(target=sorting_thread, args=(array, i))
-        p.start()
-    p.join()
+        p = multiprocessing.Process(target=sorting_thread, args=(array, i))     # p is multiprocessing thread.
+        p.start()       # start the work.
+    p.join()        # stop the work.
     print('#############End###############')
-
-    '''
-    Output
-############Start#############
-#############Thread #0 started...............
-
-#############Thread #1 started...............
-
-#############Thread #2 started...............
-
-#############Thread #3 started...............
-
-#############Thread #4 started...............
-
-#############Thread #5 started...............
-
-#############Thread #7 started...............
-
-#############Thread #6 started...............
-
-#############Thread #8 started...............
-
-#############Thread #9 started...............
-
-[2] -->  5  9  15  24  27  30  65  72  87  91  0.493 sec
-[1] -->  5  9  15  24  27  30  65  72  87  91  0.495 sec
-[0] -->  5  9  15  24  27  30  65  72  87  91  0.496 sec
-[3] -->  5  9  15  24  27  30  65  72  87  91  0.497 sec
-[5] -->  5  9  15  24  27  30  65  72  87  91  0.500 sec
-[9] -->  5  9  15  24  27  30  65  72  87  91  0.496 sec
-[4] -->  5  9  15  24  27  30  65  72  87  91  0.501 sec
-[6] -->  5  9  15  24  27  30  65  72  87  91  0.498 sec
-[7] -->  5  9  15  24  27  30  65  72  87  91  0.499 sec
-[8] -->  5  9  15  24  27  30  65  72  87  91  0.498 sec
-#############End###############
-    '''
