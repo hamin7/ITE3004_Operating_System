2803e5772a294588ba1eb0e8bba24d59145808a7
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-06-04 13:52:28 +0900

    Add files via upload

---
+++ Synchronize_Thread_in_Python/Barrier_tut.py
@@ -0,0 +1,29 @@
+#barrier_tut.py
+from random import randrange
+from threading import Barrier, Thread
+from time import ctime, sleep
+
+num = 4
+# 4 threads will need to pass this barrier to get released.
+b = Barrier(num)
+names = [“Harsh”, “Lokesh”, “George”, “Iqbal”]
+
+def player():
+    name = names.pop()
+    sleep(randrange(2, 5))
+    print(“%s reached the barrier at: %s” % (name, ctime()))
+    b.wait()
+    
+threads = []
+print(“Race starts now…”)
+
+for i in range(num):
+    threads.append(Thread(target=player))
+    threads[-1].start()
+"""
+Following loop enables waiting for the threads to complete before moving on with the main script.
+"""
+for thread in threads:
+    thread.join()
+print()
+print(“Race over!”)
